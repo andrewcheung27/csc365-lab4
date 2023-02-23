b17e8ba02569b1dfdda2b9ed84e484cb95df437e
andrewcheung27 <andrewcheung27@gmail.com>
2023-02-23 14:20:49 -0800

    fixed DATE_FORMAT for Q4 and Q6, room overlapping for Q5

--- INN-info.sql
+++ INN-info.sql
@@ -33,40 +33,37 @@ ORDER BY RoomName;
 
 
 USE `INN`;
--- Q4 TODO: date format
-SELECT CODE, TO_CHAR(CheckIn, 'MONTH DD'), TO_CHAR(CheckOut, 'MONTH DD'),
-    RoomName, DATEDIFF(CheckOut, CheckIn) * Rate AS TotalCost
+-- Q4
+SELECT CODE, RoomName, DATE_FORMAT(CheckIn, '%M %e') AS CheckIn,
+    DATE_FORMAT(CheckOut, '%M %e') AS CheckOut,
+    DATEDIFF(CheckOut, CheckIn) * Rate AS TotalCost
 FROM reservations, rooms
 WHERE reservations.Room = rooms.RoomCode
     AND FirstName = 'GRANT' AND LastName = 'KNERIEN'
-ORDER BY DAY(CheckIn);
+ORDER BY MONTH(CheckIn), DAYOFMONTH(CheckIn);
 
 
 USE `INN`;
--- Q5 TODO: overlap condition
--- SELECT rooms.RoomName, others.LastName, others.CheckIn,
-    -- DATEDIFF(others.CheckOut, others.CheckIn) AS NumNights
-SELECT rooms.RoomName, others.LastName, fritz.CheckIn AS fIn, fritz.CheckOut AS fOut, others.CheckIn, others.CheckOut, DATEDIFF(others.CheckOut, others.CheckIn) AS NumNights
+-- Q5
+SELECT rooms.RoomName, others.LastName, others.CheckIn,
+    DATEDIFF(others.CheckOut, others.CheckIn) AS NumNights
 FROM reservations AS fritz, reservations AS others, rooms
 WHERE fritz.FirstName = 'FRITZ' AND fritz.LastName = 'SPECTOR'
     AND NOT (others.FirstName = 'FRITZ' AND others.LastName = 'SPECTOR')
-    -- overlap: both check in before either checks out
-    AND others.CheckOut > fritz.CheckIn AND fritz.CheckOut > others.CheckIn
+    -- overlap: both check in before the other checks out
+    AND fritz.CheckIn + 1 < others.CheckOut
+    AND others.CheckIn + 1 < fritz.CheckOut
     AND rooms.RoomCode = others.Room AND rooms.decor = 'modern'
 ORDER BY others.CheckIn, rooms.RoomName;
 
 
 USE `INN`;
--- Query 6 TODO: date format
--- Report all reservations in rooms with double beds that contained four adults.
-For each reservation report its code, the full name and the code of the room,
-check-in and check out dates. Report reservations in chronological order,
-and sorted by the three-letter room code (in alphabetical order) for any reservations that commenced on the same day. Report check-in and check-out dates in the format 'Day Mon' (day followed by the abbreviated month), no year is required.
-
+-- Q6
 SELECT reservations.CODE, RoomCode, RoomName,
-    TO_CHAR(CheckIn, 'MONTH DD'), TO_CHAR(CheckOut, 'MONTH DD')
+    DATE_FORMAT(CheckIn, '%e %b') AS CheckIn,
+    DATE_FORMAT(CheckOut, '%e %b') AS CheckOut
 FROM reservations, rooms
 WHERE reservations.Room = rooms.RoomCode
     AND Adults = 4
     AND Beds = 2 AND bedType = 'Double'
-ORDER BY CheckIn, RoomCode;
+ORDER BY MONTH(CheckIn), DAYOFMONTH(CheckIn), RoomCode;
